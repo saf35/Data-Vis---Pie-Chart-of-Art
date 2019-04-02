ff83aa0a3d7d747fec07a8a0f379969b22e76126
saf35 <saf35@duke.edu>
2019-04-01 21:12:41 -0400

    debugging

---
+++ sketch-1.js
@@ -0,0 +1,17 @@
+/*
+New version of sketch, using to debug sketch.js
+*/
+
+var image1table;
+
+function preload(){
+    image1table = loadTable("data/image1.csv", "header");
+}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight);
+}
+
+function draw(){
+
+}
